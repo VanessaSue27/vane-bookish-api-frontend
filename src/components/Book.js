d22ac12785a0783d7a48a8b4c04dbb4cee1897f1
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-12-08 19:23:23 +0100

    Done with styling for single book section

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import bookImg from './single-book.png';
+
 // Adapt to my Book object, Fix destructuring of props in line 8, decide what
 // to show about the book in the return
 // Fix a back button?
 
 export const Book = ({ title, authors, average_rating, num_pages, isbn }) => {
   return (
-    <section>
-      <Link to="/" exact="true">
-        <p>Go Back Home</p>
-      </Link>
+    <section className="single-book-section">
+      <div className="back-link">
+        <Link to="/" exact="true">
+          Go Back Home
+        </Link>
+      </div>
 
-      <div>
-        <h1>{title}</h1>
-        <h2>{authors}</h2>
-        <p>{`Average Rating: ${average_rating}`}</p>
-        <p>{`Number of Pages: ${num_pages}`}</p>
-        <p>{`ISBN: ${isbn}`}</p>
+      <div className="book-data-container">
+        <div className="book-image">
+          <img src={bookImg} alt="books" />
+        </div>
+
+        <div className="book-data-text">
+          <h1>{title}</h1>
+          <h2>By: {authors}</h2>
+          <p>{`Average Rating: ${average_rating}`}</p>
+          <p>{`Number of Pages: ${num_pages}`}</p>
+          <p>{`ISBN: ${isbn}`}</p>
+        </div>
       </div>
     </section>
   )
