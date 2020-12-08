61257791b1533b1bb290c5d505d62ea57d7fa077
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-12-08 18:01:05 +0100

    Navigation Bar styling done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,29 +4,31 @@ import { NavLink } from 'react-router-dom';
 export const Nav = () => {
   return (
     <nav>
-      <NavLink to="/">
+      <NavLink to="/" className="bookish-text">
         Vane&apos;s Bookish App
       </NavLink>
 
-      <NavLink to="/search">
-        Search by Author
-      </NavLink>
+      <div className="nav-links">
+        <NavLink to="/search">
+          Search by Author
+        </NavLink>
 
-      <NavLink to="/top-rated">
-        Top Rated Books
-      </NavLink>
+        <NavLink to="/top-rated">
+          Top Rated Books
+        </NavLink>
 
-      <NavLink to="/reviewers-choice">
-        Reviewer&apos;s Choice
-      </NavLink>
+        <NavLink to="/reviewers-choice">
+          Reviewer&apos;s Choice
+        </NavLink>
 
-      <NavLink to="/quick-read">
-        Quick Read
-      </NavLink>
+        <NavLink to="/quick-read">
+          Quick Read
+        </NavLink>
 
-      <NavLink to="/documentation">
-        Bookish API Documentation
-      </NavLink>
+        <NavLink to="/documentation">
+          Bookish API Documentation
+        </NavLink>
+      </div>
     </nav>
   );
 };
